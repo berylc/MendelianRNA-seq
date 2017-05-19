cc08abc6dacd92ca7c4506b0c2786ada036566a9
berylc <berylc@broadinstitute.org>
2017-05-19 12:24:43 -0400

    Adding command line options to make scripts easier to run

--- QC/JointGenotype.sh
+++ QC/JointGenotype.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 gatk_jar=$1
-gvcflist=$2
+hg_fasta=$2
+gvcflist=$3
+
 java -Xmx28g -jar $gatk_jar \
 -T GenotypeGVCFs --disable_auto_index_creation_and_locking_when_reading_rods \
 -R /seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta \
--- QC/MakeGVCF.sh
+++ QC/MakeGVCF.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 gatk_jar=$1
-bam=$2
+hg_fasta=$2
+bam=$3
 
 id=`basename $bam | sed 's/.bam//'`
 java -Xmx4000m -jar $gatk_jar \
 -T HaplotypeCaller --disable_auto_index_creation_and_locking_when_reading_rods \
 -ERC GVCF --max_alternate_alleles 3 -variant_index_parameter 128000 -variant_index_type LINEAR --read_filter OverclippedRead \
 -contamination 0.0 \
--R "/path/to/Homo_sapiens_assembly19.fasta" \
+-R $hg_fasta \
 -U ALLOW_N_CIGAR_READS \
 -L ../purcell.interval_list \
 -I $bam \
