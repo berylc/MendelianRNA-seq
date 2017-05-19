4ed4bcaa00ad465a5fdd5525c8a20cc380fa4b5a
berylc <berylc@broadinstitute.org>
2017-05-19 12:21:14 -0400

    Added command line options for GATK

--- QC/JointGenotype.sh
+++ QC/JointGenotype.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-gvcflist=$1
-java -Xmx28g -jar /seq/software/picard/current/bin/GenomeAnalysisTK-3.4-g3c929b0.jar \
+gatk_jar=$1
+gvcflist=$2
+java -Xmx28g -jar $gatk_jar \
 -T GenotypeGVCFs --disable_auto_index_creation_and_locking_when_reading_rods \
 -R /seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta \
 -L ../data/purcell.interval_list \
--- QC/MakeGVCF.sh
+++ QC/MakeGVCF.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-bam=$1
+gatk_jar=$1
+bam=$2
+
 id=`basename $bam | sed 's/.bam//'`
-java -Xmx4000m -jar "/path/to/jar/gatk.jar" \
+java -Xmx4000m -jar $gatk_jar \
 -T HaplotypeCaller --disable_auto_index_creation_and_locking_when_reading_rods \
 -ERC GVCF --max_alternate_alleles 3 -variant_index_parameter 128000 -variant_index_type LINEAR --read_filter OverclippedRead \
 -contamination 0.0 \
